df440e38bd78c8b006f29775e4df8889c042ecd4
sudo0511 <shudhanshu0511max@gmail.com>
2024-08-04 19:08:11 +0530

    added dependecies

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 console.log(root);
-const ele = React.createElement('h1', {id:'heading'}, 'Hello React!!!');
+const ele = React.createElement('h1', {id:'heading'}, 'Hello React and Sudo!!!');
 console.log(ele);
 
 root.render(ele)
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,9 @@
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+console.log(root);
+const ele = React.createElement("h1", {
+    id: "heading"
+}, "Hello React and Sudo!!!");
+console.log(ele);
+root.render(ele);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
