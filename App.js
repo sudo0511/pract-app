9292dce60f332217e2b268ea1def8d4043b31515
sudo0511 <shudhanshu0511max@gmail.com>
2024-08-10 14:17:29 +0530

    added parcel and builds

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 console.log(root);
-const ele = React.createElement('h1', {id:'heading'}, 'Hello React and Sudo!!!');
+const ele = React.createElement('h1', {id:'heading'}, 'Hello React and Sudo 😂!!!');
 console.log(ele);
 
 root.render(ele)
